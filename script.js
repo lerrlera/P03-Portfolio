bde4660d0cdd37e444945bafc94045aa41c561e2
lerrlera <heyvalliet@gmail.com>
2024-04-02 13:44:01 -0700

    Style guide + hp
    updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* First, wait for DOM content to be fully loaded before executing the function 
+to ensure that script code can see the circle asnd other elements in HTML */
 document.addEventListener('DOMContentLoaded', function () {
   const circle = document.getElementById('circle');
   let mouseX = 0;
@@ -5,19 +7,23 @@ document.addEventListener('DOMContentLoaded', function () {
   let circleX = 0;
   let circleY = 0;
 
+  /* execute whenever mouse moves */
   document.addEventListener('mousemove', function (e) {
-    // Store the current mouse coordinates
     mouseX = e.pageX;
     mouseY = e.pageY;
   });
 
+  /* Updating the position of the circle based on the mouse cursor position 
+  speed is 0.08 */
   function updateCirclePosition() {
     circleX += (mouseX - circleX) * 0.08;
     circleY += (mouseY - circleY) * 0.08;
 
+    /* translate to the updated position */
     circle.style.transform = `translate(${circleX}px, ${circleY}px)`;
     circle.style.display = 'block';
   }
 
+  /* continuously update the position of the circle based on the mouse cursor. */
   setInterval(updateCirclePosition, 40); 
 });
